2f0a7edec2ad2cb225a608ef20da6ed4f34b22f5
rauleibanez <83990877+rauleibanez@users.noreply.github.com>
2024-11-19 19:52:23 -0500

    API Server, DB connect, Models and Routes

---
+++ server/routes/main.js
@@ -0,0 +1,115 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
+
+// ------ Routes ------ //
+
+/**
+ * GET - HOME
+ */
+router.get('', async (req, res) => {
+  try {
+    const locals = {
+      title: "NodeJs Blog",
+      description: "Simple Blog created with NodeJs, Express & MongoDb."
+    }
+
+    let perPage = 10;
+    let page = req.query.page || 1;
+
+    const data = await Post.aggregate([ { $sort: { createdAt: -1 } } ])
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .exec();
+
+    // Count is deprecated - please use countDocuments
+    // const count = await Post.count();
+    const count = await Post.countDocuments({});
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+    res.render('index', { 
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null,
+      currentRoute: '/'
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
+
+/**
+ * GET - POST:id
+ */
+router.get('/post/:id', async (req, res) => {
+  try {
+    let slug = req.params.id;
+    const data = await Post.findById({ _id: slug });
+
+    const locals = {
+      title: data.title ,
+      description:'Simple Blog created with NodeJs, Express & MongoDB!'
+    }
+
+    res.render('post', { locals, data });
+
+  } catch (error) {
+    console.log(error);
+  }
+});  
+
+/**
+ * POST - Search
+ */
+router.post('/search', async (req, res) => {
+  try {
+    const locals = {
+      title: "Seach",
+      description: "Simple Blog created with NodeJs, Express & MongoDb."
+    }
+
+    let searchTerm = req.body.searchTerm;
+    const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "")
+
+    const data = await Post.find({
+      $or: [
+        { title: { $regex: new RegExp(searchNoSpecialChar, 'i') }},
+        { body: { $regex: new RegExp(searchNoSpecialChar, 'i') }}
+      ]
+    });
+
+    res.render("search", {
+      data,
+      locals,
+      currentRoute: '/'
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
+
+
+router.get('/about', (req, res) => {
+  res.render('about');
+});
+
+/*
+function insertPostData(){
+  Post.insertMany([
+    {
+      title:"Building a Blog",
+      body:"this is a Body text"
+    }
+  ])
+}
+*/
+
+module.exports = router;
